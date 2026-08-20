134993ebc9235d2d8448224b58886a70519fae21
joaopguimaraes13-create <joao.p.guimaraes13@aluno.senai.br>
2026-08-20 16:52:40 +0200

    database.py

--- database.py
+++ database.py
@@ -9,11 +9,28 @@ def criar_tabela ():
 
 cursor.execute("""
 create table alunos (
-    id int primary key auto increment,
-    nome varchar (150) not null,
-    idade int not null
-    nota float)
+    id INT PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INT NOT NULL
+    nota REAL)
 
 """)
 conn.commit()
 conn.close()
+
+def cadastro_aluno(nome: str, idade, nota):
+
+    if nome.strip() == "":
+        return "O nome do aluno não pode ficar em branco. Por favor, preencha!"
+
+    elif idade > 22:
+        return "Idade acima de 22 anos"
+
+    else:
+        conn = conectar()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO alunos (nome, idade, nota) values (?, ?, ?)"), (nome, idade, nota)
+
+conn.commit()
+conn.close()
+return
